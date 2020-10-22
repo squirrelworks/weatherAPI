b64a6f10e3ce0090eb5c9d218537cc505b9fe92a
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2020-10-22 10:07:38 +0200

    one day

--- script/app.js
+++ script/app.js
@@ -1,18 +1,19 @@
 
 // get the Dom element to work within
-let app=document.getElementById("app");
+let app=document.getElementById("weatherWidget");
 
 // load your data
-loadData();
+// local pos 56.949368, 10.066459
+loadData('http://www.7timer.info/bin/api.pl?lon=10.06&lat=56.94&unit=Metric&product=civillight&output=json');
 
 
 //Load Data function
-function loadData(){
+function loadData(myUrl){
 
     console.log("fetching data");
     app.textContent="Loading data";
 
-    fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civillight&output=json')
+    fetch(myUrl)
     .then((response) => {
         //return response.text();
         return response.json();
@@ -29,5 +30,6 @@ function loadData(){
 
 // dom setup
 function buildView(myData){
-    app.textContent=myData.dataseries[0].weather;
+  let myText= 'weather type: '+myData.dataseries[0].weather+' temp min: '+myData.dataseries[0].temp2m.min+' max: '+myData.dataseries[0].temp2m.max;
+    app.textContent=myText;
 }
